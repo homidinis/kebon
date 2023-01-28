dbde0adf726f9846f6662ad9157927522c3364bb
Elizabeth Eve <74340423+homidinis@users.noreply.github.com>
2023-01-28 15:48:03 +0700

    updates

---
+++ Assets/Scripts/SceneMover.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;  
+using System.Collections.Generic;  
+using UnityEngine;  
+using UnityEngine.SceneManagement;  
+public class SceneMover: MonoBehaviour {  
+    public void Home() {  
+        SceneManager.LoadScene("Home");  
+    }  
+    public void Kebun_Tanah() {  
+        SceneManager.LoadScene("Kebun_Tanah");  
+    }  
+    public void Greenhouse() {  
+        SceneManager.LoadScene("Greenhouse");  
+    }  
+}
