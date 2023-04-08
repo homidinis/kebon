7dd9ccb27c63f60672905b4467772438c16c4309
homidinis <74340423+homidinis@users.noreply.github.com>
2023-04-08 18:45:33 +0700

    Revert "gold works, sprite changing wont work"
    This reverts commit dce495f08b43272e9dd61bf82de3ec7372304f9c.

--- Assets/Scripts/GoldHandler.cs
+++ Assets/Scripts/GoldHandler.cs
@@ -4,25 +4,33 @@ using TMPro;
 
 public class GoldHandler : MonoBehaviour
 {
-    public TextMeshProUGUI goldText; // The Text UI element to display the Gold
+    public TextMeshProUGUI goldText; // The Text UI element to display the score
 
-    private int gold; // The current Gold value
+    private int gold; // The current score value
 
     void Start()
     {
+<<<<<<< Updated upstream
         PlayerPrefs.SetInt("Gold",500);
          UpdateGoldDisplay(); // Update the Gold display
+=======
+        UpdateScoreDisplay(); // Update the score display
+>>>>>>> Stashed changes
     }
 
     void Update()
     {
-
+        UpdateScoreDisplay();
     }
 
-    // Call this method to update the Gold display with the current Gold value
-    public void UpdateGoldDisplay()
+    // Call this method to update the score display with the current score value
+    void UpdateScoreDisplay()
     {
+<<<<<<< Updated upstream
         goldText.text = "Gold: " + PlayerPrefs.GetInt("Gold"); // Update the Gold text
         Debug.Log("Updated gold! Gold: " + PlayerPrefs.GetInt("Gold"));
+=======
+        goldText.text = "Gold: " + PlayerPrefs.GetInt("Gold"); // Update the score text
+>>>>>>> Stashed changes
     }
 }
--- Assets/Scripts/PlantKangkung.cs
+++ Assets/Scripts/PlantKangkung.cs
@@ -6,25 +6,21 @@ using TMPro;
 
 public class PlantKangkung : MonoBehaviour
 {
-    int state;
+    int state = 1;
     int timer = 5;
     public int Pot;
     string[] PotArray = {"Pot1","Pot2","Pot3","Pot4"}; //put pots in an array
-    string[] StateArray = { "StatePot1", "StatePot2", "StatePot3" };
     
     GameObject childImage;
-    Image childSprite;
     GameObject childButton;
+
     // Start is called before the first frame update
     void Start()
     {
-        string potState = StateArray[Pot];
-        state = PlayerPrefs.GetInt(potState);
-
-        childImage = this.transform.GetChild(0).gameObject;  //get first child, etc
+        childImage = GameObject.Find("kkk");  //get first child, etc
         childButton = this.transform.GetChild(1).gameObject;
-
         PlayerPrefs.SetInt("Gold",500);
+<<<<<<< Updated upstream
         PlayerPrefs.Save();
         
         GoldHandler myGoldHandler = GameObject.Find("Canvas").GetComponent<GoldHandler>();
@@ -32,6 +28,8 @@ public class PlantKangkung : MonoBehaviour
         // GoldHandler updateGold = new GoldHandler(); //instantiate GoldHandler object
         // updateGold.UpdateGoldDisplay(); //and call the update func from it
 
+=======
+>>>>>>> Stashed changes
     }
 
     // Update is called once per frame
@@ -42,6 +40,7 @@ public class PlantKangkung : MonoBehaviour
     
     public void ClickHandler(int NomorPot)
     {
+<<<<<<< Updated upstream
         PlayerPrefs.SetInt("Gold",500); //debug
         string plantingSpot = PotArray[NomorPot];
 
@@ -51,10 +50,20 @@ public class PlantKangkung : MonoBehaviour
 
             Debug.Log("Gold sekarang: "+ PlayerPrefs.GetInt("Gold"));
             Debug.Log("Planting function ran. State: " + state + "Planting at: "+ plantingSpot);
+=======
+        NomorPot = Pot;
+        string plantingSpot = PotArray[NomorPot];
+
+        if(state == 1 && PlayerPrefs.HasKey(plantingSpot)==false)
+        {
+            Planting(Pot);
+            Debug.Log("Planting function ran. State: " + state + "Planting at: "+ Pot);
+>>>>>>> Stashed changes
             string occupier = PlayerPrefs.GetString(plantingSpot);    
             Debug.Log("Di pot " + plantingSpot +" ini udah ada: " + occupier);
 
         }
+<<<<<<< Updated upstream
             else if(state == 1 || PlayerPrefs.HasKey(plantingSpot)==true)
             {
                 Debug.Log("HasKey? " + PlayerPrefs.HasKey(plantingSpot));
@@ -78,11 +87,33 @@ public class PlantKangkung : MonoBehaviour
                         // string bad = Debug.Log("Error function ran. State is not 1 or 3");
                     
                     }
+=======
+        else if(state == 1 && PlayerPrefs.HasKey(plantingSpot)==true)
+        {
+             Debug.Log("HasKey? " + PlayerPrefs.HasKey(plantingSpot));
+             InvokeRepeating("PlantProgress",0f,1f);
+        }
+        else if(state == 3)
+        {
+            Harvest(NomorPot);
+            Debug.Log("Harvested! State: " + state);
+        }
+        else{
+            Debug.Log("bruh");
+             Debug.Log("State: " + state + "Planting at: "+ plantingSpot);
+            string occupier = PlayerPrefs.GetString(plantingSpot);    
+            Debug.Log("Di pot " + plantingSpot +" ini udah ada: " + occupier);
+        }
+>>>>>>> Stashed changes
         
     }
     public void Planting(int nomor_talang)
     {
+<<<<<<< Updated upstream
         PlayerPrefs.SetInt("Gold",500); //debug
+=======
+        nomor_talang = Pot;
+>>>>>>> Stashed changes
         string plantingSpot = PotArray[nomor_talang];
 
          if (PlayerPrefs.HasKey(plantingSpot)) //kalau plantingSpot udah ada tanaman, do nothing
@@ -91,6 +122,7 @@ public class PlantKangkung : MonoBehaviour
             string occupier = PlayerPrefs.GetString(plantingSpot);    
             Debug.Log("Di pot " + plantingSpot +" ini udah ada: " + occupier);
         }
+<<<<<<< Updated upstream
             else
             {
                 int gold = PlayerPrefs.GetInt("Gold");
@@ -121,34 +153,53 @@ public class PlantKangkung : MonoBehaviour
                         // tmp_text.CrossFadeAlpha(0.0f, 0.05f, false);
                         // tmp_text.enabled = false;
                         }
+=======
+        else
+        {
+            int gold = PlayerPrefs.GetInt("Gold");
+            PlayerPrefs.SetString(plantingSpot,"Kangkung"); //pot-sekian udah ada kangkungnya
+            
+            if(gold > 50) //kalau gold >50 boleh
+            { 
+                state++;
+                if(state == 1)
+                {
+                    childImage.GetComponent<Image>().sprite = Resources.Load<Sprite>("Sprites/Kangkung_1");
+                    this.GetComponent<Button>().interactable = false;
+                    InvokeRepeating("PlantProgress",0f,1f);
+                }
+            }
+            else
+            {
+            state = 0;  
+            Debug.Log("Not enough money. State: " + state + "Uang sekarang: " + PlayerPrefs.GetInt("Gold"));
+            // TextMeshPro tmp_text = GetComponent<TextMeshPro>();
+            // tmp_text.enabled = true;
+            // tmp_text.CrossFadeAlpha(0.0f, 0.05f, false);
+            // tmp_text.enabled = false;
+>>>>>>> Stashed changes
             }
     }
 
     void PlantProgress()
     {
-        string potState = StateArray[Pot];
         timer --;
         if(state == 1 && timer == 4)
         {
-            Debug.Log("State Updated! 1");
-            PlayerPrefs.SetInt(potState, state+1);
-
-           // PlayerPrefs.SetInt(potState,2);
+            state ++;
             childImage.GetComponent<Image>().sprite = Resources.Load<Sprite>("Sprites/Kangkung_1");
             // timer = 2;
         }
 
         if(state == 2 && timer == 2)
         {
-            Debug.Log("State Updated! 2");
-            PlayerPrefs.SetInt(potState, state + 1);
+            state ++;
             childImage.GetComponent<Image>().sprite = Resources.Load<Sprite>("Sprites/Kangkung_2");
             childButton.SetActive(true);
         }
         if(state == 3 && timer == 1)
         {
-            Debug.Log("State Updated! 3");
-            PlayerPrefs.SetInt(potState, state + 1);
+            state ++;
             childImage.GetComponent<Image>().sprite = Resources.Load<Sprite>("Sprites/Kangkung_3");
             childButton.SetActive(true);
         }
