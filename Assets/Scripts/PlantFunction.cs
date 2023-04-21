acd2a8dc4f6f393552dd4b172da8acc90fc4a175
Elizabeth Eve <74340423+homidinis@users.noreply.github.com>
2023-04-21 16:25:16 +0700

    fixed planting function
    todo: duplicate, check for bugs

--- Assets/Scripts/PlantFunction.cs
+++ Assets/Scripts/PlantFunction.cs
@@ -7,28 +7,37 @@ using TMPro;
 public class PlantFunction : MonoBehaviour
 {
     public int Pot;
-    int state = 0;
-    int timer = 3;
+    public int state = 0;
+
+    int timerDefaultKangkung = 3;
+    int timerDefaultPokChoy = 4;
+    int timerDefaultSelada = 5;
+    int timerDefault;
     int timerState1 = 3;
     int timerState2 = 2;
     int timerState3 = 1;
+
     string[] PotArray = { "Pot1", "Pot2", "Pot3", "Pot4" }; //put pots in an array
     string[] StateArray = { "StatePot1", "StatePot2", "StatePot3" };
     string[] TimerArray = { "TimerPot1", "TimerPot2", "TimerPot3" };
 
     GameObject childImage;
-    GameObject childButton;
+
+    GameObject NetPotChoices;
+    bool netPotChoicesBool = false;
+
     string occupier;
-    Toggle NetPot;
+    Button NetPot;
 
     // Start is called before the first frame update
     void Start()
     {
-        childImage = GameObject.Find("kkk");  //get first child, etc
+        //childImage = GameObject.Find("kkk");  //get first child, etc
+        childImage = transform.parent.gameObject.transform.GetChild(1).gameObject;
+        NetPotChoices = transform.parent.gameObject.transform.GetChild(3).gameObject;
         PlayerPrefs.SetInt("Gold", 500);
         PlayerPrefs.DeleteKey("Pot1");
-        NetPot = this.transform.parent.GetComponent<Toggle>();
-
+        NetPot = this.transform.parent.GetComponent<Button>();
     }
 
     // Update is called once per frame
@@ -37,17 +46,33 @@ public class PlantFunction : MonoBehaviour
 
     }
 
+    public void Toogle()
+    {
+        if (!netPotChoicesBool)
+        {
+            netPotChoicesBool = true;
+            NetPotChoices.SetActive(true);
+        }
+        else
+        {
+            netPotChoicesBool = false;
+            NetPotChoices.SetActive(false);
+        }
+    }
+
     public void ClickHandler(int NomorPot)
     {
         NomorPot = Pot;
         string plantingSpot = PotArray[NomorPot];
         state = PlayerPrefs.GetInt(StateArray[NomorPot]);
+        int timer = PlayerPrefs.GetInt(TimerArray[NomorPot]);
 
 
         if (state == 0 && PlayerPrefs.HasKey(plantingSpot) == false)
         {
             Planting(Pot);
-            Debug.Log("Planting function ran. State: " + state + "Planting at: " + Pot);
+            
+            Debug.Log("Planting function ran. State: " + state + "Planting at PotArray no. : " + Pot);
             string occupier = PlayerPrefs.GetString(plantingSpot);
             Debug.Log("Di pot " + plantingSpot + " sekarang udah ada: " + occupier);
 
@@ -66,7 +91,7 @@ public class PlantFunction : MonoBehaviour
 
         else
         {
-            Debug.Log("bruh");
+            Debug.Log("bruh. State: " + state);
             Debug.Log("State: " + state + "Planting at: " + plantingSpot);
             string occupier = PlayerPrefs.GetString(plantingSpot);
             Debug.Log("Di pot " + plantingSpot + " ini udah ada: " + occupier);
@@ -79,6 +104,7 @@ public class PlantFunction : MonoBehaviour
 
         occupier = "Kangkung";
         NetPot.interactable = false;
+        timerDefault = timerDefaultKangkung;
         ClickHandler(nomor_talang);
 
     }
@@ -86,18 +112,23 @@ public class PlantFunction : MonoBehaviour
     {
         occupier = "Pokchoi";
         NetPot.interactable = false;
+        timerDefault = timerDefaultPokChoy;
         ClickHandler(nomor_talang);
     }
     public void PlantSelada(int nomor_talang)
     {
         occupier = "Selada"; //set selada here jadi kode dibawah tau kalau occupier sudah jadi selada 
         NetPot.interactable = false;
+        timerDefault = timerDefaultSelada;
         ClickHandler(nomor_talang);
     }
 
 
     public void Planting(int nomor_talang)
     {
+        int state = 0;
+        int timer = timerDefault;
+        
         string plantingSpot = PotArray[nomor_talang];
 
         if (PlayerPrefs.HasKey(plantingSpot)) //kalau plantingSpot udah ada tanaman, do nothing
@@ -108,10 +139,16 @@ public class PlantFunction : MonoBehaviour
         else
         {
             int gold = PlayerPrefs.GetInt("Gold");
+
             if (gold > 50) //kalau gold >50 boleh
             {
                 PlayerPrefs.SetString(plantingSpot, "Kangkung"); //pot-sekian udah ada kangkungnya
-                PlayerPrefs.SetInt(StateArray[nomor_talang], state++); //state = 1
+                state++;
+                PlayerPrefs.SetInt(StateArray[nomor_talang], state); //state = 1 state gamau increment?
+                PlayerPrefs.SetInt(TimerArray[nomor_talang], timerDefault);
+
+                PlayerPrefs.Save();
+                Debug.Log("Started planting with state: " + state + " Timer: " + timer);
 
                 if (state == 1)
                 {
@@ -133,26 +170,35 @@ public class PlantFunction : MonoBehaviour
 
     public void PlantProgress()
     {
-
+        state = PlayerPrefs.GetInt(StateArray[Pot]);
+        int timer = PlayerPrefs.GetInt(TimerArray[Pot]);
         if (state == 1 && timer == timerState1)
         {
-            PlayerPrefs.SetInt(StateArray[Pot], state++);
+            state++;
+            PlayerPrefs.SetInt(StateArray[Pot], state);
             childImage.GetComponent<Image>().sprite = Resources.Load<Sprite>("Sprites/Kangkung_1");
+            PlayerPrefs.Save();
             // timer = 2;
         }
 
         if (state == 2 && timer == timerState2)
         {
-            PlayerPrefs.SetInt(StateArray[Pot], state++);
+            state++;
+            PlayerPrefs.SetInt(StateArray[Pot], state);
             childImage.GetComponent<Image>().sprite = Resources.Load<Sprite>("Sprites/Kangkung_2");
+            PlayerPrefs.Save();
         }
 
         if (state == 3 && timer == timerState3)
         {
-            PlayerPrefs.SetInt(StateArray[Pot], state++);
+            state++;
+            PlayerPrefs.SetInt(StateArray[Pot], state);
             childImage.GetComponent<Image>().sprite = Resources.Load<Sprite>("Sprites/Kangkung_3");
+            PlayerPrefs.Save();
         }
         timer--;
+        PlayerPrefs.SetInt(TimerArray[Pot], timer);
+        PlayerPrefs.Save();
 
         if (timer == 0 && state == 4)
         {
@@ -164,6 +210,7 @@ public class PlantFunction : MonoBehaviour
 
     public void Harvest(int nomor_talang)
     {
+        int timer = PlayerPrefs.GetInt(TimerArray[nomor_talang]);
         NetPot.interactable = true;
         if (timer == 0 && state == 4)
         {
@@ -173,13 +220,15 @@ public class PlantFunction : MonoBehaviour
                 nomor_talang = Pot;
                 string plantingSpot = PotArray[nomor_talang];
                 //RESET TIMER AND STATE, todo: insert to PlayerPrefs
-                state = 0;
+                int state = 0;
                 PlayerPrefs.SetInt(StateArray[Pot],state);
                 timer = 4;
+                PlayerPrefs.SetInt(TimerArray[Pot],timer); 
                 int lastKangkung = PlayerPrefs.GetInt("Kangkung");
                 PlayerPrefs.SetInt("Kangkung", lastKangkung + 1); //increment inventory
                 PlayerPrefs.Save();
                 PlayerPrefs.DeleteKey(plantingSpot);
+
                 childImage.GetComponent<Image>().sprite = Resources.Load<Sprite>("Sprites/kkk");
                 Debug.Log("Harvested!");
             }
@@ -229,6 +278,7 @@ public class PlantFunction : MonoBehaviour
 
     public void ResetPot()
     {
+        int timer = PlayerPrefs.GetInt(TimerArray[Pot]);
         int gold = PlayerPrefs.GetInt("Gold");
         string plantingSpot = PotArray[Pot];
         PlayerPrefs.DeleteAll();
--- Assets/Scripts/ResetPot1.cs
+++ Assets/Scripts/ResetPot1.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HarvestScript : MonoBehaviour
+public class ResetPot1 : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -14,9 +14,5 @@ public class HarvestScript : MonoBehaviour
     void Update()
     {
         
-    }
-    public void Harvest()
-    {
-
     }
 }
