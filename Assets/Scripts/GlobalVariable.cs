d21c41604bf22ce54eac8f8832ca498914182211
Elizabeth Eve <74340423+homidinis@users.noreply.github.com>
2023-07-26 14:55:06 +0700

    Global var updated

--- Assets/Scripts/GlobalVariable.cs
+++ Assets/Scripts/GlobalVariable.cs
@@ -3,14 +3,38 @@ using System.Collections.Generic;
 
 public class GlobalVariable
 {
-    public static string[,] arrayMasak =
+    public static string[,] arrayResep =
     { // 1 ke kanan, 0 ke bawah
         { "Pokchoi Siram Jamur", "Sprites/Kangkung_2", "Pokchoi x1\nBawang x1\nJamur x1\nKecap x1"},
-        { "Resep B", "Sprites/Kangkung_1","Pokchoi x1\nBawang x1\nJamur x1\nKecap x1"},
-        { "Resep C", "Sprites/Kangkung_2","Pokchoi x1\nBawang x1\nJamur x1\nKecap x1"},
+        { "Pokchoi Saus Tiram", "Sprites/Kangkung_1","Pokchoi x1\nBawang x1\nKecap x1\nSaus Tiram x1"},
+        { "Tumis Pakchoi Tahu", "Sprites/Kangkung_2","Pokchoi x1\nBawang x1\nSaus Tiram x1\nTahu x1\nCabai"},
+        
+        { "Tumis Selada Jamur", "Sprites/Kangkung_2", "Selada x1\n Bawang x1 \n Jamur x1\n Kecap x1"},
+        { "Telur Dadar Selada","Sprites/Kangkung_3","Selada x1\n Telur x1\n Kecap x1\n Margarin x1"},
 
+        { "Kangkung Tempe","Sprites/Kangkung_3","Kangkung x1 \n Bawang x1 \n Saus Tiram x1 \n Tempe x1" },
+        { "Cah Kangkung","Sprites/Kangkung_3","Kangkung x1 \n Bawang x1 \n Saus Tiram x1\n Cabai x1"},
+
+        { "Jagung Bakar", "Sprite/Kangkung_3","Jagung x1\n Margarin x1" },
+        { "Tumis Jagung Bakar","Sprite/Kangkung_3","Jagung Bakar x1\n Bawang x1\n Cabai x1" },
+        { "Jagung Jamur Asam Manis","Sprite/Kangkung_3","Jagung x1 \n Jamur x1 \n Saus Tiram x1 \n Cabai x1 \n"},
+        { "Sup Jagung","Sprite/Kangkung_3","Jagung x1 \n Bawang x1 \n Telur x1 \n Tepung x1"},
+
+        { "Cincin Bawang","Sprite/Kangkung_3","Bawang x1 \n Tepung x1 \n Minyak x1"},
+
+        { "Tumis Edamame","Sprite/Kangkung_3", "Edamame x1 \n Bawang x1 \n Kecap x1 \n Margarin x1"},
+        { "Edamame Goreng","Sprite/Kangkung_3" , "Edamame x1 \n Bawang x1 \n Minyak x1 \n"},
+
+        { "Nasi Goreng Edamame","Sprite/Kangkung_3" , "Nasi x1 \n Edamame x1 \n Bawang x1 \n Kecap x1 \n Cabai x1 \n Minyak x1"},
+        { "Nasi Goreng Jagung","Sprite/Kangkung_3" , "Nasi x1 \n Jagung x1 \n Bawang x1 \n Kecap x1 \n Cabai x1 \n Minyak x1"},
+
+        { "Telur Rebus","Sprite/Kangkung_3","Telur x1" },
+        { "Telur Ceplok","Sprite/Kangkung_3","Telur x1 \n Minyak x1" },
+        { "Telur Dadar","Sprite/Kangkung_3" , "Telur x1 \n Bawang x1 \n Minyak x1 \n Saus Tiram x1"},
+
+        { "Nasi Goreng Lengkap","Sprite/Kangkung_3","Nasi x1 \n Tumis Pokchoi Tahu x1 \n Selada x1 \n Kangkung x1 \n Jagung x1 \n Edamame x1 \n Bawang x1 \n Telur x1 \n Jamur x1 \n Kecap x1 \n Saus Tiram x1 \n Cabai x1 \n Minyak x1 \n" }
     };
-    public static string[][] jaggedArray =
+    public static string[][] arrayIngredient =
     {
         new string[]
             {"Pokchoi","Bawang","Jamur","Kecap"},
@@ -37,6 +61,26 @@ public class GlobalVariable
             {"Jagung","Bawang","SausTiram","Tempe"},
         new string[]
             {"Jagung","Bawang","SausTiram","Cabai"},
+        
+        new string[]
+            {"Cincin Bawang","Bawang","Tepung","Minyak"},
+        
+        new string[]
+            {"Tumis Edamame","Bawang","SausTiram","Cabai"},
+        new string[]
+            {"Edamame goreng","Bawang","SausTiram","Cabai"},
+
+        new string[]
+            {"Telur Rebus","Bawang","SausTiram","Cabai"},
+        new string[]
+            {"Telur Ceplok","Bawang","SausTiram","Cabai"},
+        new string[]
+            {"Telur Dadar","Bawang","SausTiram","Cabai"},
+        
+        new string[]
+            {"Nasi Goreng Lengkap","Tumis Pokchoi Tahu","Selada","Kangkung Tempe","Jagung","Edamame","Nasi","Bawang","Telur Ceplok","Jamur","Kecap","SausTiram","Cabai","Minyak"}
+
+
     };
     public static string[,] arrayMarket =
     { // 1 ke kanan, 0 ke bawah
--- Assets/Scripts/Masak.cs
+++ Assets/Scripts/Masak.cs
@@ -19,8 +19,9 @@ public class Masak : MonoBehaviour
     void Start()
     {
         Debug.Log(PlayerPrefs.GetInt("Susu"));
-        Debug.Log("Get Length 1: " + GlobalVariable.arrayMasak.GetLength(1) + " Get Length 0: " + GlobalVariable.arrayMasak.GetLength(0));
-        for (int i = 0; i < GlobalVariable.arrayMasak.GetLength(0); i++)
+        Debug.Log("Get Length 1: " + GlobalVariable.arrayResep.GetLength(1) + " Get Length 0: " + GlobalVariable.arrayResep.GetLength(0));
+        Debug.Log(GlobalVariable.arrayResep.GetLength(0));
+        for (int i = 0; i < GlobalVariable.arrayResep.GetLength(0); i++)
         {
             GameObject myObj = Instantiate(btnObject, new Vector2(0, 0), Quaternion.identity);
             
@@ -28,17 +29,18 @@ public class Masak : MonoBehaviour
 
             int iCopy = i;
             myObjButton.onClick.AddListener(() => OpenRecipe(iCopy));
+            Debug.Log("Listener added " + iCopy);
 
             myObj.transform.parent = canvasObject.transform;
             myObj.transform.localScale = new Vector3(1, 1, 1);
 
-            //myObjButton.onClick.AddListener(delegate { Buy(i); });
+            //myObjButton.onClick.AddListener(delegate { Buy(i); });/
 
             TextMeshProUGUI myObjNameText = myObj.transform.GetChild(0).GetComponent<TMPro.TextMeshProUGUI>();
-            myObjNameText.text = GlobalVariable.arrayMasak[i, 0];
+            myObjNameText.text = GlobalVariable.arrayResep[i, 0];
 
             Image myObjImage = myObj.transform.Find("Image").GetComponent<Image>();
-            myObjImage.sprite = Resources.Load<Sprite>(GlobalVariable.arrayMasak[i, 1]);
+            myObjImage.sprite = Resources.Load<Sprite>(GlobalVariable.arrayResep[i, 1]);
         }
     }
 
@@ -51,47 +53,49 @@ public class Masak : MonoBehaviour
     {
         recipePanel.SetActive(false);
         recipeIngredientsPanel.SetActive(true);
-        recipeTitle.text = GlobalVariable.arrayMasak[j, 0];
-        recipeDescription.text = GlobalVariable.arrayMasak[j, 2];
+        recipeTitle.text = GlobalVariable.arrayResep[j, 0];
+        recipeDescription.text = GlobalVariable.arrayResep[j, 2];
         Button myMasakButton = masakButton.GetComponent<Button>();
         myMasakButton.onClick.RemoveAllListeners();
         myMasakButton.onClick.AddListener(() => MasakFunction(j));
+        Debug.Log("Recipe opened");
     }
     void MasakFunction(int i)
     {
-        Debug.Log(GlobalVariable.jaggedArray[i].Length);
+        Debug.Log(GlobalVariable.arrayIngredient[i].Length);
         bool readyToCook = true;
-        for(int j = 0; j < GlobalVariable.jaggedArray[i].Length; j++)
+        for(int j = 0; j < GlobalVariable.arrayIngredient[i].Length; j++)
         {
-            int bahan = PlayerPrefs.GetInt(GlobalVariable.jaggedArray[i][j]);
+            int bahan = PlayerPrefs.GetInt(GlobalVariable.arrayIngredient[i][j]);
             if(bahan <= 0)
             {
                 readyToCook = false;
+                Debug.Log("Bahan kurang!" + bahan);
             }
         }
         if(readyToCook)
         {
-            for(int j = 0; j < GlobalVariable.jaggedArray[i].Length; j++)
+            for(int j = 0; j < GlobalVariable.arrayIngredient[i].Length; j++)
             {
-                int bahan = PlayerPrefs.GetInt(GlobalVariable.jaggedArray[i][j]);
-                bahan--;
-                PlayerPrefs.SetInt(GlobalVariable.jaggedArray[i][j], bahan);
+                int bahan = PlayerPrefs.GetInt(GlobalVariable.arrayIngredient[i][j]); 
+                bahan--; 
+                PlayerPrefs.SetInt(GlobalVariable.arrayIngredient[i][j], bahan); //reduce bahan by 1 and set playerpref of that ingredient
                 //open cooking animation
-                int jumlahMakanan = PlayerPrefs.GetInt(GlobalVariable.arrayMasak[i, 0]);
+                int jumlahMakanan = PlayerPrefs.GetInt(GlobalVariable.arrayResep[i, 0]); //resep yang bahannya masakan lain bagaimana?
                 jumlahMakanan++;
-                PlayerPrefs.SetInt(GlobalVariable.arrayMasak[i, 0], jumlahMakanan);
-                Debug.Log(GlobalVariable.arrayMasak[i, 0] + ' ' + jumlahMakanan);
+                PlayerPrefs.SetInt(GlobalVariable.arrayResep[i, 0], jumlahMakanan);
+                Debug.Log(GlobalVariable.arrayResep[i, 0] + ' ' + jumlahMakanan);
             
             }
         } else
         {
-            Debug.Log("Bahan kurang");
+            Debug.Log("Something went wrong: readytocook: " + readyToCook); ;
         }
     }
     void Buy(int j) //onclick: check gold: if price < gold then increment inventory. add "yakin membeli" button
     {
-        int price = int.Parse(GlobalVariable.arrayMasak[j, 2]);
-        string name = GlobalVariable.arrayMasak[j, 0];
+        int price = int.Parse(GlobalVariable.arrayResep[j, 2]);
+        string name = GlobalVariable.arrayResep[j, 0];
         int Gold = PlayerPrefs.GetInt("Gold");
         if(Gold >= price)
         {
